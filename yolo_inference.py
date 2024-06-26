aa672675d0d3189c09307500d26182b20fb0bb8e
Kamen Zranchev <kamenzranchev@gmail.com>
2024-06-26 17:01:23 +0300

    sync

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,7 +1,6 @@
 
 from ultralytics import YOLO
 
-
 model = YOLO('yolov8x')
 
 results = model.predict('input_videos/08fd33_4.mp4', save=True)
@@ -9,3 +8,4 @@ print(results[0])
 print('==============================================')
 for box in results[0].boxes:
     print(box)
+    
